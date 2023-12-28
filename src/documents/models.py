510a09250d78454e1d158b9c7e0f03a2865b9d2a
Trenton Holmes <797416+stumpylog@users.noreply.github.com>
2023-12-28 09:16:37 -0800

    Fixes new linting errors

--- src/documents/models.py
+++ src/documents/models.py
@@ -394,11 +394,6 @@ class Log(models.Model):
 
 
 class SavedView(ModelWithOwner):
-    class Meta:
-        ordering = ("name",)
-        verbose_name = _("saved view")
-        verbose_name_plural = _("saved views")
-
     name = models.CharField(_("name"), max_length=128)
 
     show_on_dashboard = models.BooleanField(
@@ -415,6 +410,11 @@ class SavedView(ModelWithOwner):
         blank=True,
     )
     sort_reverse = models.BooleanField(_("sort reverse"), default=False)
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = _("saved view")
+        verbose_name_plural = _("saved views")
 
 
 class SavedViewFilterRule(models.Model):
